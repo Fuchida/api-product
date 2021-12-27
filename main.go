b0e9aa91e1227f06227040acdaa8b3a25f131bce
abdul <abdul@podsights.com>
2021-12-26 20:59:37 -0600

    feat: update product and go fmt

--- main.go
+++ main.go
@@ -29,24 +29,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
 type product struct {
-	ID string `json:"id"`
-	Name string `json:"name"`
-	Price float64 `json:"price"`
-	Quantity int32 `json:"quantity"`
+	ID       string  `json:"id"`
+	Name     string  `json:"name"`
+	Price    float64 `json:"price"`
+	Quantity int32   `json:"quantity"`
 }
 
 // removes a product from  inventory
 func removeProductByIndex(index int) {
 	products = append(products[:index], products[index+1:]...)
-	
+
 }
 
 // check if a products exists in the  inventory of products
 // TODO: update to return bool for existance and index of product
 // 		will enable other functions like deleteProduct to not loop twice
-func productExists(p product, items[] product) bool {
+func productExists(p product, items []product) bool {
 	for _, product := range items {
 		if product.ID == p.ID {
 			return true
@@ -60,27 +59,26 @@ var products = []product{
 	{ID: "18", Name: "Office Desk", Price: 200.00, Quantity: 5000},
 }
 
-func addProduct(c *gin.Context){
+func addProduct(c *gin.Context) {
 	var newProduct product
-	
+
 	if err := c.BindJSON(&newProduct); err != nil {
 		return
 	}
 
-	if productExists(newProduct, products){
+	if productExists(newProduct, products) {
 		c.IndentedJSON(http.StatusConflict, "product already exists")
-		return  
-		
+		return
+
 	} else {
 		products = append(products, newProduct)
-		c.IndentedJSON(http.StatusAccepted, newProduct)	
+		c.IndentedJSON(http.StatusAccepted, newProduct)
 	}
 }
 
-	
 func getProductByID(c *gin.Context) {
 	id := c.Param("id")
-	
+
 	for _, product := range products {
 		if product.ID == id {
 			c.IndentedJSON(http.StatusOK, product)
@@ -91,12 +89,11 @@ func getProductByID(c *gin.Context) {
 	c.IndentedJSON(http.StatusNotFound, message)
 }
 
-
-func getProducts(c *gin.Context){
+func getProducts(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, products)
 }
 
-func deleteProduct(c *gin.Context){
+func deleteProduct(c *gin.Context) {
 	id := c.Param("id")
 	for index, product := range products {
 		if product.ID == id {
@@ -111,11 +108,37 @@ func deleteProduct(c *gin.Context){
 	c.IndentedJSON(http.StatusNotFound, message)
 }
 
+func updateProduct(c *gin.Context) {
+	var patch product
+
+	if err := c.BindJSON(&patch); err != nil {
+		return
+	}
+
+	if productExists(patch, products) {
+		for index, product := range products {
+			if patch.ID == product.ID {
+				products[index] = patch
+
+				// TODO: can we log the full representation of the product ?
+				message := fmt.Sprintf("Product: %s, has been updated", product.ID)
+
+				c.IndentedJSON(http.StatusAccepted, message)
+				return
+			}
+		}
+	} else {
+		message := fmt.Sprintf("Product id: %s, was not found ", patch.ID)
+		c.IndentedJSON(http.StatusNotFound, message)
+	}
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/products", getProducts)
-	router.GET("/product/:id", getProductByID)	
+	router.GET("/product/:id", getProductByID)
 	router.POST("/product", addProduct)
+	router.PATCH("/product/", updateProduct)
 	router.DELETE("product/:id", deleteProduct)
 	router.Run("localhost:7000")
 }
