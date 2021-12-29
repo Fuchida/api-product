0d4561228a6cae91b3943af6aee2969e29eb6e7a
abdul <abdul@podsights.com>
2021-12-29 15:28:34 -0600

    test: adds initial test for products route

---
+++ main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestProductsRoute(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/products", nil)
+	router.ServeHTTP(w, req)
+	
+	expected := 200
+	received := w.Code
+
+	testLog := "Testing /products route"
+    testError := fmt.Sprintf("Listing products failed verification expected:%d but recieved: %d", expected, received)
+    
+    if expected != received {
+        t.Logf(testLog)
+        t.Errorf(testError)
+    }
+}
