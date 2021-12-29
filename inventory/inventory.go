211b653e018af66e9bd92937b4db602869bc60ab
abdul <abdul@podsights.com>
2021-12-29 14:11:49 -0600

    feat: adds ability to clear all inventory

--- inventory/inventory.go
+++ inventory/inventory.go
@@ -9,11 +9,11 @@ var products = []domain.Product{}
 
 // create initial stock for inventory
 func Bootstrap() []domain.Product {
-	 products = []domain.Product{
-		{ID: "21", Name: "Nintendo Switch Lite", Price: 399.90, Quantity: 2000},
-		{ID: "18", Name: "Office Desk", Price: 200.00, Quantity: 5000},
-	}
-	return products
+     products = []domain.Product{
+        {ID: "21", Name: "Nintendo Switch Lite", Price: 399.90, Quantity: 2000},
+        {ID: "18", Name: "Office Desk", Price: 200.00, Quantity: 5000},
+    }
+    return products
 }
 
 /* check if a products exists in the  inventory of products
@@ -23,42 +23,52 @@ func Exists(product domain.Product) bool {
 TODO: update to return bool for existance and index of product
 	will enable other functions like deleteProduct to not loop twice
 */
-	for _, item := range List() {
-		if reflect.DeepEqual(item, product) {
-			return true
-		}
-	}
-	return false
+    for _, item := range List() {
+        if reflect.DeepEqual(item, product) {
+            return true
+        }
+    }
+    return false
 }
 
 // list the current available inventory
 func List() []domain.Product {
-	return products
+    return products
 }
 
 // adds a new product to the inventory
 func Add(product domain.Product) domain.Product {
-	products = append(products, product	)
-	
-	return product
+    products = append(products, product	)
+    
+    return product
 }
 
 // updates an exsting inventory item
 func Update(update domain.Product) domain.Product {
-	for index, product := range List() {
-		if update.ID == product.ID {
-			products[index] = update
+    // TODO: update func name to UpdateByID
+    for index, product := range List() {
+        if update.ID == product.ID {
+            products[index] = update
 
-			return update
-		}
-	}
+            return update
+        }
+    }
+    // MARK: Do we return an error when we fail to update ? 
 	// MARK: Do we return an error when we fail to update ? 
-	// 		 currently we just return an empty zero value Product
-	return domain.Product{}
+    // MARK: Do we return an error when we fail to update ? 
+    // 		 currently we just return an empty zero value Product
+    return domain.Product{}
 }
 
 // removes the item from inventory
 func Remove(index int) int {
-	products = append(products[:index], products[index+1:]...)
-	return index
+    products = append(products[:index], products[index+1:]...)
+    return index
+}
+
+// removes all items from inventory
+func removeAll(){
+    // MARK: RemoveAll is here as clean up for my tests but not used
+    //       in other places. Is there a better way ?
+    products = []domain.Product{}
 }
