5baf58a435effaf27d09a8e4de44b585d5d038b2
abdul <abdul@podsights.com>
2021-12-29 14:12:26 -0600

    feat: adds tests for remaining inventory public api

--- inventory/inventory_test.go
+++ inventory/inventory_test.go
@@ -2,29 +2,158 @@ package inventory
 
 import (
 	"api_product/domain"
+	"fmt"
 	"testing"
 )
 
-var _ []domain.Product = Bootstrap()
+func Test_Boostrap(t *testing.T){
+    expected := 2
+
+    products = Bootstrap()
+    received := len(products)
+    defer removeAll()
+    
+    if received != expected {
+        t.Logf("Testing boostaping the inventory")
+        t.Errorf("Boostrap verification failed expected: %d but got: %d",
+                expected, received)
+    }
+}
 
 func Test_Exists(t *testing.T){
-	// validating existance
-	nintendoSwitch := domain.Product{ID: "21", Name: "Nintendo Switch Lite", Price: 399.90, Quantity: 2000}
-	want := true
-	got := Exists(nintendoSwitch)
-	
-	if want != got {
-		t.Logf("Testing existance of %s", nintendoSwitch.Name)
-		t.Errorf("Existance verification failed want: %t but got: %t", want, got)
-	}
-	// validating non-existace
-	sonyPlaystation := domain.Product{ID: "22", Name: "Sony Playstation", Price: 399.90, Quantity: 2000}
-	want = false
-	got = Exists(sonyPlaystation)
-	
-	if want != got {
-		t.Logf("Testing existance of %s", sonyPlaystation.Name)
-		t.Errorf("Existance verification failed want: %t but got: %t", want, got)
-	}
-	
+    _ = Bootstrap()
+    defer removeAll()
+    
+    // validating existance
+    nintendoSwitch := domain.Product{ID: "21", Name: "Nintendo Switch Lite", Price: 399.90, Quantity: 2000}
+    expected := true
+    recieved := Exists(nintendoSwitch)
+
+    if expected != recieved {
+        t.Logf("Testing existance of %s", nintendoSwitch.Name)
+        t.Errorf("Existance verification failed expected: %t but recieved: %t", expected, recieved)
+    }
+    // validating non-existace
+    sonyPlaystation := domain.Product{ID: "22", Name: "Sony Playstation", Price: 399.90, Quantity: 2000}
+    expected = false
+    recieved = Exists(sonyPlaystation)
+
+    if expected != recieved {
+        t.Logf("Testing existance of %s", sonyPlaystation.Name)
+        t.Errorf("Existance verification failed expected: %t but recieved: %t", expected, recieved)
+    }
+
+}
+
+func Test_List(t *testing.T){
+    expected := 0
+    received := len(List())
+    
+    if expected != received {
+        t.Logf("Testing listing of inventory items")
+        t.Errorf("Listing verification failed expected: %d but recieved: %d", expected, received)
+    }
+    
+    _ = Bootstrap()
+    defer removeAll()
+    expected = 2
+    received = len(List())
+    
+    testLog := "Testing listing of inventory items"
+    testError := fmt.Sprintf("Listing verification failed expected: %d but recieved: %d", expected, received)
+    
+    if expected != received {
+        t.Logf(testLog)
+        t.Errorf(testError)
+    }
+}
+
+func Test_Add(t *testing.T){
+    expected := true
+    
+    _ = Bootstrap()
+    defer removeAll()
+    
+    segaGenesis := domain.Product{
+        ID: "55",
+        Name: "Sega Genesis",
+        Price: 299.00,
+        Quantity: 56,
+        
+    }
+    
+    _ = Add(segaGenesis)
+    received := Exists(segaGenesis)
+    
+    testLog := "Testing adding an item to the inventory"
+    testError := fmt.Sprintf("Inventory addition verification failed expected: %t but recieved: %t", expected, received)
+    
+    if expected != received {
+        t.Logf(testLog)
+        t.Errorf(testError)
+    }
+}
+
+func Test_Update(t *testing.T) {
+    _ = Bootstrap()
+    defer removeAll()
+    
+    expected := true
+    
+    nintendoSwitchPro := domain.Product{ID: "21", Name: "Nintendo Switch Pro", Price: 399.90, Quantity: 3000}
+    
+    _ = Update(nintendoSwitchPro)
+    
+    received := Exists(nintendoSwitchPro)
+    
+    testLog := "Testing updating an inventory item"
+    testError := fmt.Sprintf("Inventory upate verification failed expected: %t but recieved: %t", expected, received)
+    
+    if expected != received {
+        t.Logf(testLog)
+        t.Errorf(testError)
+    }
+    
+}
+
+func Test_Remove(t *testing.T){
+    _ = Bootstrap()
+    defer removeAll()
+    
+    expected := false
+    SegaGameGear := domain.Product{ID: "33", Name: "Sega Game Gear", Price: 99.90, Quantity: 6000}
+    
+    _ = Add(SegaGameGear)
+    for index, item := range List() {
+        if item.ID == SegaGameGear.ID{
+            Remove(index)
+        }
+    }
+    received := Exists(SegaGameGear)
+    
+        
+    testLog := "Testing removing an inventory item"
+    testError := fmt.Sprintf("Inventory removal verification failed expected: %t but recieved: %t", expected, received)
+    
+    if expected != received {
+        t.Logf(testLog)
+        t.Errorf(testError)
+    }
+}
+
+func Test_RemoveAll(t *testing.T){
+    _ = Bootstrap()
+    expected := 0
+    
+    removeAll()
+    received :=  len(List())
+    
+    testLog := "Testing removing an inventory item"
+    testError := fmt.Sprintf("Inventory removal verification failed expected: %d but recieved: %d", expected, received)
+    
+    if expected != received {
+        t.Logf(testLog)
+        t.Errorf(testError)
+    }
+    
 }
