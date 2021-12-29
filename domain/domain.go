207ccbeeb4a595d8f4f171c43fb471f05ede3ea8
abdul <abdul@podsights.com>
2021-12-29 14:11:07 -0600

    docs: formating on model

--- domain/domain.go
+++ domain/domain.go
@@ -2,8 +2,8 @@ package domain
 
 
 type Product struct {
-	ID       string  `json:"id"`
-	Name     string  `json:"name"`
-	Price    float64 `json:"price"`
-	Quantity int32   `json:"quantity"`
+    ID       string  `json:"id"`
+    Name     string  `json:"name"`
+    Price    float64 `json:"price"`
+    Quantity int32   `json:"quantity"`
 }
