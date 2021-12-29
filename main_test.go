b64535534ca26f09daa8cb49ef97ef09bb5234b8
abdul <abdul@podsights.com>
2021-12-29 15:28:58 -0600

    lint: applies go fmt

--- main_test.go
+++ main_test.go
@@ -13,15 +13,15 @@ func TestProductsRoute(t *testing.T) {
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/products", nil)
 	router.ServeHTTP(w, req)
-	
+
 	expected := 200
 	received := w.Code
 
 	testLog := "Testing /products route"
-    testError := fmt.Sprintf("Listing products failed verification expected:%d but recieved: %d", expected, received)
-    
-    if expected != received {
-        t.Logf(testLog)
-        t.Errorf(testError)
-    }
+	testError := fmt.Sprintf("Listing products failed verification expected:%d but recieved: %d", expected, received)
+
+	if expected != received {
+		t.Logf(testLog)
+		t.Errorf(testError)
+	}
 }
